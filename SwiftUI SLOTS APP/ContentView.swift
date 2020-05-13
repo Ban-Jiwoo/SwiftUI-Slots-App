210f2ddb804fa98d7eefed59f95c5bcedad60fb4
Ian Jiwoo Ban <jban365@gmail.com>
2020-05-14 00:36:06 +0900

    Initial Commit

---
+++ SwiftUI SLOTS APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI SLOTS APP
+//
+//  Created by Jiwoo Ban on 2020/05/14.
+//  Copyright © 2020 Jiwoo Ban. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
